20087f8a7345f266e24c8d1ef5e857b551559653
karthik_bs <166012406+itsmekarthikbs@users.noreply.github.com>
2025-08-09 20:47:42 +0530

    Update bitops.c

--- Task2_RISCV_Setup/bitops.c
+++ Task2_RISCV_Setup/bitops.c
@@ -1,42 +1,14 @@
-#include <stdio.h>
 #include "unique.h"
 
-unsigned int set_bit(unsigned int num, int pos) {
-    return num | (1U << pos);
-}
+int main(void) {
+    uniq_print_header("bitops"); // Prints identity info
 
-unsigned int clear_bit(unsigned int num, int pos) {
-    return num & ~(1U << pos);
-}
-
-unsigned int toggle_bit(unsigned int num, int pos) {
-    return num ^ (1U << pos);
-}
-
-int check_bit(unsigned int num, int pos) {
-    return (num >> pos) & 1U;
-}
-
-int main() {
-    unsigned int num = 0b1010; // binary 10
-    printf("Initial: %u\n", num);
-
-    num = set_bit(num, 1);
-    printf("After setting bit 1: %u\n", num);
-
-    num = clear_bit(num, 3);
-    printf("After clearing bit 3: %u\n", num);
-
-    num = toggle_bit(num, 0);
-    printf("After toggling bit 0: %u\n", num);
-
-    printf("Bit 2 is %s\n", check_bit(num, 2) ? "SET" : "CLEAR");
-
-    printf("User: %s\n", USERNAME);
-    printf("Host: %s\n", HOSTNAME);
-    printf("Machine ID: %s\n", MACHINE_ID);
-    printf("Timestamp: %s\n", TIMESTAMP);
-    printf("Epoch Time: %s\n", EPOCH_TIME);
+    unsigned x = 0xA5A5A5A5u, y = 0x0F0F1234u;
+    printf("x&y=0x%08X\n", x & y);
+    printf("x|y=0x%08X\n", x | y);
+    printf("x^y=0x%08X\n", x ^ y);
+    printf("x<<3=0x%08X\n", x << 3);
+    printf("y>>2=0x%08X\n", y >> 2);
 
     return 0;
 }
